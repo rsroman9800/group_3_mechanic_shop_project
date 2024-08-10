c7b91f84ae69ae389e9a5390afb18698d1920473
N-Asadi <asadi.n2000@gmail.com>
2024-08-10 03:53:08 -0600

    CPRG 211 Final Group Assignment: Complete Front-End Solution
    This repository houses the entire front-end implementation for the Final Group Assignment of CPRG 211, located in the Main branch

---
+++ CPRG_211_Final_Project/FinalProject/WrenchHub/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
